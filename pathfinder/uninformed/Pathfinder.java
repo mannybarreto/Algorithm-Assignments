f3c32dbb369b0f366833a3456a4c9a85d7c405c4
Manuel Barreto <you@example.com>
2019-01-22 22:11:07 -0800

    Miliano's changes

--- pathfinder/uninformed/Pathfinder.java
+++ pathfinder/uninformed/Pathfinder.java
@@ -1,6 +1,7 @@
 package pathfinder.uninformed;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
@@ -10,12 +11,12 @@ import java.util.Queue;
  * Maze Pathfinding algorithm that implements a basic, uninformed, breadth-first tree search.
  */
 public class Pathfinder {
-    
+
     /**
      * Given a MazeProblem, which specifies the actions and transitions available in the
      * search, returns a solution to the problem as a sequence of actions that leads from
      * the initial to a goal state.
-     * 
+     *
      * @param problem A MazeProblem that specifies the maze, actions, transitions.
      * @return An ArrayList of Strings representing actions that lead from the initial to
      * the goal state, of the format: ["R", "R", "L", ...]
@@ -23,43 +24,54 @@ public class Pathfinder {
     public static ArrayList<String> solve (MazeProblem problem) {
         // TODO: Initialize frontier -- what data structure should you use here for
         // breadth-first search? Recall: The frontier holds SearchTreeNodes!
-        Queue<SearchTreeNode> frontier = new LinkedList<>();
-                
-        // TODO: Add new SearchTreeNode representing the problem's initial state to the
+    	Queue<SearchTreeNode> frontier = new LinkedList<>();
+
+    	// TODO: Add new SearchTreeNode representing the problem's initial state to the
         // frontier. Since this is the initial state, the node's action and parent will
         // be null
         frontier.add(new SearchTreeNode(problem.INITIAL_STATE, null, null));
-        
+
         // TODO: Loop: as long as the frontier is not empty...
         while (!frontier.isEmpty()) {
-        
             // TODO: Get the next node to expand by the ordering of breadth-first search
-            Map<String, MazeState> possibleTransitions = problem.getTransitions(frontier.peek().state);
-            
-            for (Map.Entry<String, MazeState> action : possibleTransitions.entrySet()) {
-                MazeState actionMod = action.getValue();
-                frontier.add(new SearchTreeNode(actionMod, action.getKey(), frontier.peek()));
-            }
-            
+        	SearchTreeNode node = frontier.remove();
+           	Map<String, MazeState> possibleTransitions = problem.getTransitions(node.state);
+
             // TODO: If that node's state is the goal (see problem's isGoal method),
             // you're done! Return the solution
             // [Hint] Use a helper method to collect the solution from the current node!
-            
+            if (problem.isGoal(node.state)) {
+            	return getSolution(node);
+            }
+
             // TODO: Otherwise, must generate children to keep searching. So, use the
             // problem's getTransitions method from the currently expanded node's state...
-            
+
             // TODO: ...and *for each* of those transition states...
             // [Hint] Look up how to iterate through <key, value> pairs in a Map -- an
             // example of this is already done in the MazeProblem's getTransitions method
-        
+            for (Map.Entry<String, MazeState> action : possibleTransitions.entrySet()) {
                 // TODO: ...add a new SearchTreeNode to the frontier with the appropriate
                 // action, state, and parent
-        
+                frontier.add(new SearchTreeNode(action.getValue(), action.getKey(), node));
+            }
         }
         // Should never get here, but just return null to make the compiler happy
         return null;
     }
-    
+
+    public static ArrayList<String> getSolution(SearchTreeNode goal) {
+    	ArrayList<String> result = new ArrayList<>();
+
+    	while (goal.parent != null) {
+    		result.add(goal.action);
+    		goal = goal.parent;
+    	}
+
+    	Collections.reverse(result);
+
+    	return result;
+    }
 }
 
 /**
@@ -67,14 +79,14 @@ public class Pathfinder {
  * tree.
  */
 class SearchTreeNode {
-    
+
     MazeState state;
     String action;
     SearchTreeNode parent;
-    
+
     /**
      * Constructs a new SearchTreeNode to be used in the Search Tree.
-     * 
+     *
      * @param state The MazeState (col, row) that this node represents.
      * @param action The action that *led to* this state / node.
      * @param parent Reference to parent SearchTreeNode in the Search Tree.
@@ -84,5 +96,5 @@ class SearchTreeNode {
         this.action = action;
         this.parent = parent;
     }
-    
+
 }
