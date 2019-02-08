b0120f2a26382755d53da7673ee2e64c7c04ec03
Manuel Barreto <you@example.com>
2019-02-07 17:11:16 -0800

    Changes 1-4 in Problem 1

--- pathfinder/informed/MazeProblem.java
+++ pathfinder/informed/MazeProblem.java
@@ -137,14 +137,15 @@ public class MazeProblem {
      * 
      * @param possibleSoln A possible solution to test, which is a list of actions of the format:
      * ["U", "D", "D", "L", ...]
-     * @return A 2-element array of ints of the format [isSoln, cost] where:<br>
-     * isSoln will be 0 if it is not a solution, and 1 if it is<br>
+     * @return A 2-element array of ints of the format [isSoln, cost] where:
+     * isSoln will be 0 if it is not a solution, and 1 if it is
      * cost will be an integer denoting the cost of the given solution to test optimality
      */
     public int[] testSolution (ArrayList<String> possibleSoln) {
         // Update the "moving state" that begins at the start and is modified by the transitions
         MazeState movingState = new MazeState(INITIAL_STATE.col, INITIAL_STATE.row);
         int cost = 0;
+        boolean hasKey = false;
         int[] result = {0, -1};
         
         // For each action, modify the movingState, and then check that we have landed in
@@ -152,14 +153,30 @@ public class MazeProblem {
         for (String action : possibleSoln) {
             MazeState actionMod = TRANS_MAP.get(action);
             movingState.add(actionMod);
-            if (maze[movingState.row].charAt(movingState.col) == 'X') {
+            switch (maze[movingState.row].charAt(movingState.col)) {
+            case 'X':
                 return result;
+            case 'K':
+                hasKey = true; break;
             }
-            cost++;
+            cost += getCost(movingState);
         }
-        result[0] = isGoal(movingState) ? 1 : 0;
+        result[0] = isGoal(movingState) && hasKey ? 1 : 0;
         result[1] = cost;
         return result;
+    }
+    
+    public int getCost (MazeState state) { 
+        char tile = maze[state.row].charAt(state.col);
+        
+        if (tile == 'M') {
+            return 3;
+        } else if (tile == '.' || tile == 'I' || tile == 'K' || tile == 'G') {
+            return 1;
+        } else {
+            throw new IllegalArgumentException();
+        }
+        
     }
     
 }
