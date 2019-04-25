ec88e0c99deae9578874c7b7b21e5ae1dc3b4576
Manuel Barreto <you@example.com>
2019-04-24 23:09:45 -0700

    Added Josh's tests and empty string handling

--- huffman/Huffman.java
+++ huffman/Huffman.java
@@ -12,14 +12,13 @@ import java.io.ByteArrayOutputStream;
  * distributions of characters.
  */
 public class Huffman {
-    
     // -----------------------------------------------
     // Construction
     // -----------------------------------------------
 
     private HuffNode trieRoot;
     private Map<Character, String> encodingMap;
-    
+
     /**
      * Creates the Huffman Trie and Encoding Map using the character
      * distributions in the given text corpus
@@ -30,27 +29,28 @@ public class Huffman {
      *        differ.
      */
     Huffman (String corpus) {
+        if (corpus == "") {throw new IllegalArgumentException(); }
+        
         PriorityQueue<HuffNode> queue = createPriorityQueue(corpus);
         constructTrie(queue);
         encodingMap = new HashMap<Character, String>();
         retrieveEncoding(trieRoot, "");
     }
-    
-    
+
     /*
      * Core methods for Huffman creation, compression, and decompression.
      */
-    
+
     /**
      * Takes the inputed String and counts the appearances of characters to create a PriorityQueue of
      * HuffNodes and their # of appearances. Queue generally used for constructing the tree used for
      * finding encodingMap.
-     * @param message String to be used 
+     * @param message String to be used
      * @return PriorityQueue<HuffNode> containing all characters and their number of appearances in message.
      */
     private PriorityQueue<HuffNode> createPriorityQueue(String message) {
         HashMap<Character, Integer> distribution = new HashMap<>();
-        
+
         for (int i = 0; i < message.length(); i++) {
             char currentChar = message.charAt(i);
             if (distribution.containsKey(currentChar)) {
@@ -59,18 +59,18 @@ public class Huffman {
                 distribution.put(currentChar, 1);
             }
         }
-        
+
         PriorityQueue<HuffNode> result = new PriorityQueue<HuffNode>();
-        
+
         for (Entry<Character, Integer> entry : distribution.entrySet()) {
             result.add(new HuffNode(entry.getKey(), entry.getValue()));
         }
-        
+
         return result;
     }
-    
+
     /**
-     * Takes queue formed from createPriorityQueue and creates Huffman Trie, 
+     * Takes queue formed from createPriorityQueue and creates Huffman Trie,
      * setting trieRoot to last element in queue.
      * @param queue PriorityQueue formed by createPriorityQueue with HuffmanNodes of each character in message.
      */
@@ -80,7 +80,7 @@ public class Huffman {
             newNode.left = queue.poll();
             queue.add(newNode);
         }
-        
+
         while (queue.size() > 1) {
             HuffNode newNode = new HuffNode((char) 0, 0);
 
@@ -93,6 +93,12 @@ public class Huffman {
         trieRoot = queue.poll();
     }
 
+    /**
+     * Retrieves Huffman Encoding used for optimal compression using binary nodes
+     * that construct the Huffman Trie and the given path.
+     * @param node that is binary and contains a character and count field.
+     * @param path that is the length characters on Huffman Trie.
+     */
     private void retrieveEncoding(HuffNode node, String path) {
         if (node.isLeaf()) {
             encodingMap.put(node.character, path);
@@ -106,130 +112,132 @@ public class Huffman {
             retrieveEncoding(node.right, path + "1");
         }
     }
-    
+
     // -----------------------------------------------
-    // Compression
-    // -----------------------------------------------
-    
-    /**
-     * Compresses the given String message / text corpus into its Huffman coded
-     * bitstring, as represented by an array of bytes. Uses the encodingMap
-     * field generated during construction for this purpose.
-     * @param message String representing the corpus to compress.
-     * @return {@code byte[]} representing the compressed corpus with the
-     *         Huffman coded bytecode. Formatted as 3 components: (1) the
-     *         first byte contains the number of characters in the message,
-     *         (2) the bitstring containing the message itself, (3) possible
-     *         0-padding on the final byte.
-     */
-    public byte[] compress (String message) {
-        String encodedString = "";
-        ByteArrayOutputStream bout = new ByteArrayOutputStream(); 
-        
-        for (int i = 0; i < message.length(); i++) {
-            encodedString = encodedString + encodingMap.get(message.charAt(i));
-        }
-        
-        while (encodedString.length() % 8 != 0) {
-            encodedString = encodedString + "0";
-        }
-        
-        for (int i = 0; i < encodedString.length() / 8; i++) {
-            String currentByte = encodedString.substring(8 * i, 8 * (i + 1));
-            bout.write(Integer.parseInt(currentByte, 2));
-        }     
-                
-        byte[] encoded = bout.toByteArray();
-        byte[] result = new byte[1 + encoded.length];
-        result[0] = (byte) message.length();
-        
-        for (int i = 0; i < encoded.length; i++) {
-            result[1 + i] = encoded[i];
-        }
-        
-        return result;
-    }
-     
-    
-    // -----------------------------------------------
-    // Decompression
-    // -----------------------------------------------
-    
-    /**
-     * Decompresses the given compressed array of bytes into their original,
-     * String representation. Uses the trieRoot field (the Huffman Trie) that
-     * generated the compressed message during decoding.
-     * @param compressedMsg {@code byte[]} representing the compressed corpus with the
-     *        Huffman coded bytecode. Formatted as 3 components: (1) the
-     *        first byte contains the number of characters in the message,
-     *        (2) the bitstring containing the message itself, (3) possible
-     *        0-padding on the final byte.
-     * @return Decompressed String representation of the compressed bytecode message.
-     */
-    public String decompress (byte[] compressedMsg) {
-        String decodedString = "";
-        HuffNode currentNode = trieRoot;
-        String strCompressedMsg = "";
-        
-        for (int k = 1; k < compressedMsg.length; k++) {
-            String currentByte = "";
-            if (compressedMsg[k] < 0) {
-                currentByte = Integer.toBinaryString(compressedMsg[k]).substring(24);
-            } else {
-                currentByte = Integer.toBinaryString(compressedMsg[k]);
-            }
-   
-            while (currentByte.length() < 8) {
-                currentByte = "0" + currentByte;
-            }
-            
-            strCompressedMsg += currentByte;
-        }
-        
-        for (int i = 0; decodedString.length() < compressedMsg[0]; i++) {
-            if (currentNode.isLeaf() == true) {
-                decodedString += currentNode.character;
-                currentNode = trieRoot;
-            }
-            if (strCompressedMsg.charAt(i) == '1') {
-                currentNode = currentNode.right;
-            } else {
-                currentNode = currentNode.left;
-            }
-        }
-        return decodedString;
-    }
-    
-    // -----------------------------------------------
-    // Huffman Trie
-    // -----------------------------------------------
-    
-    /**
-     * Huffman Trie Node class used in construction of the Huffman Trie.
-     * Each node is a binary (having at most a left and right child), contains
-     * a character field that it represents (in the case of a leaf, otherwise
-     * the null character \0), and a count field that holds the number of times
-     * the node's character (or those in its subtrees) appear in the corpus.
-     */
-    private static class HuffNode implements Comparable<HuffNode> {
-        
-        HuffNode left, right;
-        char character;
-        int count;
-        
-        HuffNode (char character, int count) {
-            this.count = count;
-            this.character = character;
-        }
-        
-        public boolean isLeaf () {
-            return left == null && right == null;
-        }
-        
-        public int compareTo (HuffNode other) {
-            return this.count - other.count;
-        }
-        
-    }
+   // Compression
+   // -----------------------------------------------
+
+   /**
+    * Compresses the given String message / text corpus into its Huffman coded
+    * bitstring, as represented by an array of bytes. Uses the encodingMap
+    * field generated during construction for this purpose.
+    * @param message String representing the corpus to compress.
+    * @return {@code byte[]} representing the compressed corpus with the
+    *         Huffman coded bytecode. Formatted as 3 components: (1) the
+    *         first byte contains the number of characters in the message,
+    *         (2) the bitstring containing the message itself, (3) possible
+    *         0-padding on the final byte.
+    */
+   public byte[] compress (String message) {
+       if (message == "") {throw new IllegalArgumentException(); }
+       
+       String encodedString = "";
+       ByteArrayOutputStream bout = new ByteArrayOutputStream();
+
+       for (int i = 0; i < message.length(); i++) {
+           encodedString = encodedString + encodingMap.get(message.charAt(i));
+       }
+
+       while (encodedString.length() % 8 != 0) {
+           encodedString = encodedString + "0";
+       }
+
+       for (int i = 0; i < encodedString.length() / 8; i++) {
+           String currentByte = encodedString.substring(8 * i, 8 * (i + 1));
+           bout.write(Integer.parseInt(currentByte, 2));
+       }
+
+       byte[] encoded = bout.toByteArray();
+       byte[] result = new byte[1 + encoded.length];
+       result[0] = (byte) message.length();
+
+       for (int i = 0; i < encoded.length; i++) {
+           result[1 + i] = encoded[i];
+       }
+
+       return result;
+   }
+
+
+   // -----------------------------------------------
+   // Decompression
+   // -----------------------------------------------
+
+   /**
+    * Decompresses the given compressed array of bytes into their original,
+    * String representation. Uses the trieRoot field (the Huffman Trie) that
+    * generated the compressed message during decoding.
+    * @param compressedMsg {@code byte[]} representing the compressed corpus with the
+    *        Huffman coded bytecode. Formatted as 3 components: (1) the
+    *        first byte contains the number of characters in the message,
+    *        (2) the bitstring containing the message itself, (3) possible
+    *        0-padding on the final byte.
+    * @return Decompressed String representation of the compressed bytecode message.
+    */
+   public String decompress (byte[] compressedMsg) {
+       String decodedString = "";
+       HuffNode currentNode = trieRoot;
+       String strCompressedMsg = "";
+
+       for (int k = 1; k < compressedMsg.length; k++) {
+           String currentByte = "";
+           if (compressedMsg[k] < 0) {
+               currentByte = Integer.toBinaryString(compressedMsg[k]).substring(24);
+           } else {
+               currentByte = Integer.toBinaryString(compressedMsg[k]);
+           }
+
+           while (currentByte.length() < 8) {
+               currentByte = "0" + currentByte;
+           }
+
+           strCompressedMsg += currentByte;
+       }
+
+       for (int i = 0; decodedString.length() < compressedMsg[0]; i++) {
+           if (currentNode.isLeaf() == true) {
+               decodedString += currentNode.character;
+               currentNode = trieRoot;
+           }
+           if (strCompressedMsg.charAt(i) == '1') {
+               currentNode = currentNode.right;
+           } else {
+               currentNode = currentNode.left;
+           }
+       }
+       return decodedString;
+   }
+
+   // -----------------------------------------------
+   // Huffman Trie
+   // -----------------------------------------------
+
+   /**
+    * Huffman Trie Node class used in construction of the Huffman Trie.
+    * Each node is a binary (having at most a left and right child), contains
+    * a character field that it represents (in the case of a leaf, otherwise
+    * the null character \0), and a count field that holds the number of times
+    * the node's character (or those in its subtrees) appear in the corpus.
+    */
+   private static class HuffNode implements Comparable<HuffNode> {
+
+       HuffNode left, right;
+       char character;
+       int count;
+
+       HuffNode (char character, int count) {
+           this.count = count;
+           this.character = character;
+       }
+
+       public boolean isLeaf () {
+           return left == null && right == null;
+       }
+
+       public int compareTo (HuffNode other) {
+           return this.count - other.count;
+       }
+
+   }
 
 }
--- huffman/HuffmanTests.java
+++ huffman/HuffmanTests.java
@@ -12,20 +12,20 @@ public class HuffmanTests {
         // byte 0: 0000 0010 = 2 (message length = 2)
         // byte 1: 0100 0000 = 64 (0 = "A", 1 = "B")
         // [!] Only first 2 bits of byte 1 are meaningful
-        byte[] compressed = {2, 64};
+        byte[] compressed = { 2, 64 };
         assertArrayEquals(compressed, h.compress("AB"));
     }
-    
+
     @Test
     public void comp_t1() {
         Huffman h = new Huffman("AB");
         // byte 0: 0000 0010 = 2 (message length = 2)
         // byte 1: 1000 0000 = -128 (0 = "A", 1 = "B")
         // [!] Only first 2 bits of byte 1 are meaningful
-        byte[] compressed = {2, -128};
+        byte[] compressed = { 2, -128 };
         assertArrayEquals(compressed, h.compress("BA"));
     }
-    
+
     @Test
     public void comp_t2() {
         Huffman h = new Huffman("ABBBCC");
@@ -33,39 +33,67 @@ public class HuffmanTests {
         // byte 1: 1000 0111 = -121 (10 = "A", 0 = "B", C = 11)
         // byte 2: 1000 0000 = -128
         // [!] Only first bit of byte 2 is meaningful
-        byte[] compressed = {6, -121, -128};
+        byte[] compressed = { 6, -121, -128 };
         assertArrayEquals(compressed, h.compress("ABBBCC"));
     }
-    
+
     @Test
     public void comp_t3() {
         Huffman h = new Huffman("ABBBCC");
         // byte 0: 0000 0110 = 6 (message length = 6)
         // byte 1: 0100 1101 = 77 (10 = "A", 0 = "B", C = 11)
         // byte 2: 1000 0000 = -128
-        byte[] compressed = {6, 77, -128};
+        byte[] compressed = { 6, 77, -128 };
         assertArrayEquals(compressed, h.compress("BABCBC"));
     }
-    
+
     @Test
     public void comp_t4() {
         Huffman h = new Huffman("DDDD");
         // byte 0: 0000 0100
         // byte 1: 0000 0000
-        byte[] compressed = {4, 0};
+        byte[] compressed = { 4, 0 };
         assertArrayEquals(compressed, h.compress("DDDD"));
     }
-    
+
     @Test
     public void comp_t5() {
         Huffman h = new Huffman("GAAAAEEEEEDDDFF");
-        // byte 0: 
-        // byte 1:
         
-        byte[] compressed = {15, 85, 95, -8, 13, -128};
+        byte[] compressed = { 15, 85, 95, -8, 13, -128 };
         assertArrayEquals(compressed, h.compress("GAAAAEEEEEDDDFF"));
     }
+
+    @Test
+    public void comp_t6() {
+        Huffman h = new Huffman(" ");
+        // byte 0: 0000 0001
+        // byte 1: 0000 0000
+        byte[] compressed = { 1, 0 };
+        assertArrayEquals(compressed, h.compress(" "));
+    }
+
+    @Test
+    public void comp_t7() {
+        Huffman h = new Huffman("BA");
+        // byte 0: 0000 0010 = 2 (message length = 2)
+        // byte 1: 0100 0000 = 64 (0 = "A", 1 = "B")
+        // [!] Only first 2 bits of byte 1 are meaningful
+        byte[] compressed = { 2, 64 };
+        assertArrayEquals(compressed, h.compress("AB"));
+    }
+
+    @Test
+    public void comp_t8() {
+        Huffman h = new Huffman("BA");
+        // byte 0: 0000 0010 = 2 (message length = 2)
+        // byte 1: 1000 0000 = -128 (0 = "A", 1 = "B")
+        // [!] Only first 2 bits of byte 1 are meaningful
+        byte[] compressed = { 2, -128 };
+        assertArrayEquals(compressed, h.compress("BA"));
+    }
     
+
     // Decompression Tests
     // -----------------------------------------------
     @Test
@@ -73,37 +101,64 @@ public class HuffmanTests {
         Huffman h = new Huffman("AB");
         // byte 0: 0000 0010 = 2 (message length = 2)
         // byte 1: 0100 0000 = 64 (0 = "A", 1 = "B")
-        byte[] compressed = {2, 64};
+        byte[] compressed = { 2, 64 };
         assertEquals("AB", h.decompress(compressed));
     }
-    
+
     @Test
     public void decomp_t1() {
         Huffman h = new Huffman("AB");
         // byte 0: 0000 0010 = 2 (message length = 2)
         // byte 1: 1000 0000 = -128 (0 = "A", 1 = "B")
-        byte[] compressed = {2, -128};
+        byte[] compressed = { 2, -128 };
         assertEquals("BA", h.decompress(compressed));
     }
-    
+
     @Test
     public void decom_t3() {
         Huffman h = new Huffman("ABBBCC");
         // byte 0: 0000 0110 = 6 (message length = 6)
         // byte 1: 1000 0111 = -121 (10 = "A", 0 = "B", C = 11)
         // byte 2: 1000 0000 = -128
-        byte[] compressed = {6, -121, -128};
+        byte[] compressed = { 6, -121, -128 };
         assertEquals("ABBBCC", h.decompress(compressed));
     }
-    
+
     @Test
     public void decom_t4() {
         Huffman h = new Huffman("ABBBCC");
         // byte 0: 0000 0110 = 6 (message length = 6)
         // byte 1: 0100 1101 = 77 (10 = "A", 0 = "B", C = 11)
         // byte 2: 1000 0000 = -128
-        byte[] compressed = {6, 77, -128};
+        byte[] compressed = { 6, 77, -128 };
         assertEquals("BABCBC", h.decompress(compressed));
     }
-    
+
+    @Test
+    public void decom_t5() {
+        Huffman h = new Huffman(" ");
+        // byte 0: 0000 0001 = 1
+        // byte 1: 0000 0000
+        byte[] compressed = { 1, 0 };
+        assertEquals(" ", h.decompress(compressed));
+    }
+
+    @Test
+    public void decomp_t6() {
+        Huffman h = new Huffman("BA");
+        // byte 0: 0000 0010 = 2 (message length = 2)
+        // byte 1: 0100 0000 = 64 (0 = "A", 1 = "B")
+        byte[] compressed = { 2, 64 };
+        assertEquals("AB", h.decompress(compressed));
+    }
+
+    @Test
+    public void decomp_t7() {
+        Huffman h = new Huffman("BA");
+        // byte 0: 0000 0010 = 2 (message length = 2)
+        // byte 1: 1000 0000 = -128 (0 = "A", 1 = "B")
+        byte[] compressed = { 2, -128 };
+        assertEquals("BA", h.decompress(compressed));
+    }
+
 }
