da56064df380abe03b11600cfa673c64b18e0723
Manuel Barreto <you@example.com>
2019-05-08 17:39:12 -0700

    Added helper methods and base solve/backtrack behavior

--- csp/CSP.java
+++ csp/CSP.java
@@ -24,7 +24,65 @@ public class CSP {
      *         indexed by the variable they satisfy, or null if no solution exists.
      */
     public static List<LocalDate> solve (int nMeetings, LocalDate rangeStart, LocalDate rangeEnd, Set<DateConstraint> constraints) {
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
+        List<LocalDate> assignment = new List<LocaleDate>();
+        DateVar probDomain = new DateVar(rangeStart, rangeEnd);
+        return backtrack(assignment, nMeetings, probDomain, constraints);
     }
-    
+
+    // TODO
+    private static List<LocalDate> backtrack(List<LocalDate> assignment, int nMeetings, DateVar probDomain, Set<DateConstraint> constraints) {
+        if (assignment.size() == nMeetings) {
+            return assignment;
+        }
+
+        List<Integer> unassignedVars = getUnassaignedVars(assignment, nMeetings);
+
+        for (LocalDate current = probDomain.rangeStart; current.isBefore(probDomain.rangeEnd); current = current.plusDays(1)) {
+            
+        }
+
+        return null;
+    }
+
+
+    // Helper Methods / Classes
+    /**
+     * Helper function that returns a list of variables that have yet to be assigned
+     * in the assignment list.
+     * @param assignment List of assignments currently made
+     * @param nMeetings number of meetings
+     */
+    private static List<Integer> getUnassignedVars(List<LocalDate> assignment, int nMeetings) {
+        List<Integer> result = new List<Integer>();
+        for (int i = 0; i < nMeetings; i++) {
+            if (assignment.get(i) == null) {
+                result.add(i);
+            }
+        }
+    }
+
+    public static class DateVar {
+        LocalDate rangeStart;
+        LocalDate rangeEnd;
+
+        public DateVar (LocalDate rangeStart, LocalDate rangeEnd) {
+            this.rangeStart = rangeStart;
+            this.rangeEnd = rangeEnd;
+        }
+    }
+
+    public static boolean checkConsistency (LocalDate leftDate, LocalDate rightDate, DateConstraint constraint) {
+        boolean isConsistent = false;
+        switch (constraint.OP) {
+            case "==": if (leftDate.isEqual(rightDate))  isConsistent = true; break;
+            case "!=": if (!leftDate.isEqual(rightDate)) isConsistent = true; break;
+            case ">":  if (leftDate.isAfter(rightDate))  isConsistent = true; break;
+            case "<":  if (leftDate.isBefore(rightDate)) isConsistent = true; break;
+            case ">=": if (leftDate.isAfter(rightDate) || leftDate.isEqual(rightDate))  isConsistent = true; break;
+            case "<=": if (leftDate.isBefore(rightDate) || leftDate.isEqual(rightDate)) isConsistent = true; break;
+        }
+        
+        return isConsistent;
+    }
 }
