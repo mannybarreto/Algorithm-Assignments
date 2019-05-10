d707fc38df2b211e06d3797cbc1c976a4b79332d
Manuel Barreto <you@example.com>
2019-05-09 17:36:17 -0700

    Finished assignment

--- csp/CSP.java
+++ csp/CSP.java
@@ -3,6 +3,8 @@ package csp;
 import java.time.LocalDate;
 import java.util.Set;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Queue;
 
@@ -26,35 +28,45 @@ public class CSP {
      *         indexed by the variable they satisfy, or null if no solution exists.
      */
     public static List<LocalDate> solve (int nMeetings, LocalDate rangeStart, LocalDate rangeEnd, Set<DateConstraint> constraints) {
-        List<DateVar> domains = new ArrayList<LocalDate>(nMeetings);
+        List<DateVar> domains = new ArrayList<DateVar>(nMeetings);
         initializeDomains(domains, nMeetings, rangeStart, rangeEnd);
         
         for (DateConstraint constraint : constraints) {
             if (constraint.arity() == 1) {
-                nodeConsistency(constraint, domains);
+                UnaryDateConstraint castedConstraint = (UnaryDateConstraint) constraint;
+                nodeConsistency(castedConstraint, domains);
+            } else {
+                BinaryDateConstraint castedConstraint = (BinaryDateConstraint) constraint;
+                constrainWithArcConsistency(castedConstraint, domains);
             }
         }
 
         List<LocalDate> assignment = new ArrayList<LocalDate>();
         initializeAssignment(assignment, nMeetings);
         
-
-        DateVar probDomain = new DateVar(rangeStart, rangeEnd);
-        return backtrack(assignment, nMeetings, probDomain, constraints);
+        return backtrack(assignment, nMeetings, domains, constraints);
     }
 
-    private static List<LocalDate> backtrack(List<LocalDate> assignment, int nMeetings, DateVar probDomain, Set<DateConstraint> constraints) {
+    /**
+     * Recursively backtracks using constrained domains until solution is found.
+     * @param assignment Current set of assigned vars
+     * @param nMeetings Numbers of meetings
+     * @param domains List containing the DataVars with each vars domains
+     * @param constraints Set of contraints given in the problem
+     * @return Completed assignment
+     */
+    private static List<LocalDate> backtrack(List<LocalDate> assignment, int nMeetings, List<DateVar> domains, Set<DateConstraint> constraints) {
         if (!assignment.contains(null)) {
             return assignment;
         }
 
         int unassignedVarIndex = getUnassignedVar(assignment, nMeetings);
 
-        for (LocalDate currentDate = probDomain.rangeStart; currentDate.isBefore(probDomain.rangeEnd) || currentDate.isEqual(probDomain.rangeEnd); currentDate = currentDate.plusDays(1)) {
-            assignment.set(unassignedVarIndex, currentDate);
+        for (LocalDate date : domains.get(unassignedVarIndex).domain) {
+            assignment.set(unassignedVarIndex, date);
             
             if (checkAssignmentConsistency(assignment, constraints)) {
-                List<LocalDate> result = backtrack(assignment, nMeetings, probDomain, constraints);
+                List<LocalDate> result = backtrack(assignment, nMeetings, domains, constraints);
                 if (result != null) {
                     return result;
                 }
@@ -66,32 +78,133 @@ public class CSP {
         return null;
     }
     
-    // public static nodeConsistency(){
-    //     for(int i = 0; i < variables.size(); i++){
-    //         for(int q = 0; q < variables.get(i).domain.size(); q++){
-    //             if(variables.get(i).domain.get(q) conflicts with unary constraint){
-    //                 remove variables.get(i).domain.get(q);
-    //             }
-    //         }
-    //     }
-    // }
+    public static void nodeConsistency(UnaryDateConstraint constraint, List<DateVar> domains) {
+        HashSet<LocalDate> domain = copyDomain(domains.get(constraint.L_VAL).domain);
+        for (LocalDate date : domains.get(constraint.L_VAL).domain) {
+            switch (constraint.OP) {
+                case "==": if (!date.isEqual(constraint.R_VAL))   domain.remove(date); break;
+                case "!=": if (date.isEqual(constraint.R_VAL))  domain.remove(date); break;
+                case ">":  if (date.isBefore(constraint.R_VAL) || date.isEqual(constraint.R_VAL))  domain.remove(date); break;
+                case "<":  if (date.isAfter(constraint.R_VAL) || date.isEqual(constraint.R_VAL))  domain.remove(date); break;
+                case ">=": if (date.isBefore(constraint.R_VAL)) domain.remove(date); break;
+                case "<=": if (date.isAfter(constraint.R_VAL)) domain.remove(date); break;
+            }
+        }
+        domains.set(constraint.L_VAL, new DateVar(domain));
+    }
+    
+    public static void constrainWithArcConsistency(BinaryDateConstraint constraint, List<DateVar> domains) {
+        HashSet<LocalDate> leftDomain = copyDomain(domains.get(constraint.L_VAL).domain);
+        HashSet<LocalDate> rightDomain = domains.get(constraint.R_VAL).domain;
+        
+        if (rightDomain.isEmpty()) {
+            return;
+        }
+        
+        for (LocalDate leftDate : domains.get(constraint.L_VAL).domain) {
+            Boolean found = false;
+            switch (constraint.OP) {
+                case "==": 
+                    leftDomain.retainAll(rightDomain);
+                    return;
+                case "!=":
+                    found = false;
+                    for (LocalDate rightDate : rightDomain) {
+                        if (!leftDate.isEqual(rightDate)) {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found) { leftDomain.remove(leftDate); }
+                    break;
+                case ">":
+                    found = false;
+                    for (LocalDate rightDate : rightDomain) {
+                        if (leftDate.isAfter(rightDate)) {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found) { leftDomain.remove(leftDate); }
+                    break;
+                case "<":
+                    found = false;
+                    for (LocalDate rightDate : rightDomain) {
+                        if (leftDate.isBefore(rightDate)) {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found) { leftDomain.remove(leftDate); }
+                    break;
+                case ">=":
+                    found = false;
+                    for (LocalDate rightDate : rightDomain) {
+                        if (leftDate.isAfter(rightDate) || leftDate.isEqual(rightDate)) {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found) { leftDomain.remove(leftDate); }
+                    break;
+                case "<=":
+                    found = false;
+                    for (LocalDate rightDate : rightDomain) {
+                        if (leftDate.isBefore(rightDate) || leftDate.isEqual(rightDate)) {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found) { leftDomain.remove(leftDate); }
+                    break;
+            }
+        }
+        domains.get(constraint.L_VAL).domain = leftDomain;
+    }
 
 
     // Helper Methods / Classes
+    /**
+     * Fills assignment List variable with nulls.
+     * @param assignment List to be filled
+     * @param size Number of assignments needing to be made
+     */
     public static void initializeAssignment(List<LocalDate> assignment, int size) {
         for (int i = 0; i < size; i++) {
             assignment.add(null);
         }
     }
 
+    /**
+     * Initializes the domains variable with all dates by days from rangeStart to rangeEnd
+     * @param domains List to be filled with domain
+     * @param size  Size of the list
+     * @param rangeStart Where the initial domains begin
+     * @param rangeEnd Where the initial domains end
+     */
     public static void initializeDomains(List<DateVar> domains, int size, LocalDate rangeStart, LocalDate rangeEnd) {
         for (int i = 0; i < size; i++) {
-            List<LocalDate> newDomain = new ArrayList<LocalDate>();
-            for (LocalDate currentDate = probDomain.rangeStart; currentDate.isBefore(probDomain.rangeEnd) || currentDate.isEqual(probDomain.rangeEnd); currentDate = currentDate.plusDays(1)) {
+            HashSet<LocalDate> newDomain = new HashSet<LocalDate>();
+            for (LocalDate currentDate = rangeStart; currentDate.isBefore(rangeEnd) || currentDate.isEqual(rangeEnd); currentDate = currentDate.plusDays(1)) {
                 newDomain.add(currentDate);
             }   
             domains.add(new DateVar(newDomain));
         }
+    }
+    
+    /**
+     * Copies HashSet into a new one to prevent concurrency errors
+     * @param domain to be copied
+     * @return New HashSet copy of the domain
+     */
+    public static HashSet<LocalDate> copyDomain(HashSet<LocalDate> domain) {
+        HashSet<LocalDate> result = new HashSet<LocalDate>();
+        
+        for (LocalDate date : domain) {
+            result.add(date);
+        }
+        
+        return result;
     }
 
     /**
@@ -109,13 +222,6 @@ public class CSP {
         return 0;
     }
 
-    public static class DateVar {
-        List<LocalDate> domain;
-
-        public DateVar (List<LocalDate> domain) {
-            this.domain = domain;
-        }
-    }
 
     /**
      * Checks if two dates pass a constrant. leftDate constraint rightDate == true?
@@ -138,6 +244,12 @@ public class CSP {
         return isConsistent;
     }
     
+    /**
+     * Iterates through all the constraints checking if given assignment is valid.
+     * @param assignment Current assignments to be checked for consistency
+     * @param constraints List of constraints used for checking
+     * @return True or False whether or not assignments are consistent
+     */
     public static boolean checkAssignmentConsistency(List<LocalDate> assignment, Set<DateConstraint> constraints){
         for (DateConstraint constraint : constraints){
             if (constraint.arity() == 2) {
@@ -160,5 +272,12 @@ public class CSP {
         return true;
     }
 
+    public static class DateVar {
+        public HashSet<LocalDate> domain;
+        
+        public DateVar (HashSet<LocalDate> domain) {
+            this.domain = domain;
+        }
+    }
     
 }
